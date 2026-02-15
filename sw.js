fe2ad744773b9315954ea3ba8d7da23daa827b1e
greg-s718 <75401756+greg-s718@users.noreply.github.com>
2026-02-14 21:20:25 -0800

    Corrected Chapter Titles

---
+++ sw.js
@@ -0,0 +1,39 @@
+// Service Worker for Guía Prematrimonial
+// Enables offline functionality
+
+const CACHE_NAME = 'guia-prematrimonial-v1';
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Update Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
